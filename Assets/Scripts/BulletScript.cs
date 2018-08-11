fa61663e3d346efe2d88478466a21445fd95d8e1
Matthew Wilmsmeyer <mwilmsmeyer@revolutionehr.com>
2018-08-11 01:27:31 -0500

    BLU-2 Enemy now takes damage, which is stored on the bullet currently (should probably move to the gun when we add more of those). -mw

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+
+    public int bulletDamage = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
