13b7baebcde40d9d2d16f2022968496d7f39c32d
xrabohrok <westin007@gmail.com>
2018-08-12 22:13:20 -0500

    added music and player - Mr.Black

---
+++ Assets/ArbitraryScripts/LevelPersist.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelPersist : MonoBehaviour
+{
+    public bool attachToMainCamera;
+    public List<string> ValidSceneNames;
+
+    void OnEnable()
+    {
+        SceneManager.activeSceneChanged += ChangedActiveScene;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.activeSceneChanged -= ChangedActiveScene;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+        DontDestroyOnLoad(gameObject);
+	}
+
+    private void ChangedActiveScene(Scene current, Scene next)
+    {
+        var stayAlive = false;
+        foreach (var validSceneName in ValidSceneNames)
+        {
+            stayAlive |= validSceneName.Equals(next.name);
+            Debug.Log("next:" + next.name + " current: " + current.name);
+        }
+
+        if (attachToMainCamera)
+        {
+            this.gameObject.transform.parent = Camera.main.transform;
+        }
+
+        if (!stayAlive)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
